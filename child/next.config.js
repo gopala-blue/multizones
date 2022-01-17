b1cdcf1b5fd7015b62ac35384b2abbd9a3b69075
gopala-blue <agrawalgopala4@gmail.com>
2022-01-18 00:54:14 +0530

    fix Node Env File

--- child/next.config.js
+++ child/next.config.js
@@ -1,5 +1,5 @@
 /** @type {import('next').NextConfig} */
-const isProd = process.enc.NODE_ENV === 'production'
+const isProd = process.env.NODE_ENV === 'production'
 const nextConfig = {
   //reactStrictMode: true,
    assetPrefix: isProd? 'https://mz-child.vercel.app' :'http://localhost:3001' 
