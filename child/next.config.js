0269020868447cc2e1e5d7db581e9497d187b377
gopala-blue <agrawalgopala4@gmail.com>
2022-01-18 10:06:30 +0530

    click test with asset prefix

--- child/next.config.js
+++ child/next.config.js
@@ -2,7 +2,7 @@
 const isProd = process.env.NODE_ENV === 'production'
 const nextConfig = {
   //reactStrictMode: true,
- //  assetPrefix: isProd? 'https://mz-child.vercel.app' :'http://localhost:3001' 
+    assetPrefix: isProd? 'https://mz-child.vercel.app' :'http://localhost:3001' 
 }
 
 module.exports = nextConfig
